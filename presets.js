65e6098961507023ec7fb84b98f4d8205e06be8f
innocentroad <innocentroad122@gmail.com>
2026-04-30 20:30:20 +0900

    fix: Include missing favicon and preset files, add cache buster

---
+++ presets.js
@@ -0,0 +1,27 @@
+// Trickcal Damage Calculator - Preset Data
+// You can easily add new characters or enemies here.
+
+const presets = {
+    atk: {
+        'lily_v15': { atk: 67107, crit: 30000, critDmg: 30000 }
+    },
+    def: {
+        'lily_v15': { def: 67107, critRes: 30000, critDmgRes: 30000 }
+    }
+};
+
+const ENEMY_PRESETS = {
+    "lily_v15": {
+        name: "[次元15]リリー(活発)",
+        hp: 661796770,
+        atk_p: 0,
+        atk_m: 28118,
+        def_p: 52196,
+        def_m: 52196,
+        dmgType: 'mag',
+        crit: 44178,
+        critDmg: 44178,
+        critRes: 36146,
+        critDmgRes: 36146
+    }
+};
